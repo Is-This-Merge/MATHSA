e9e5294bd6615b5cce52a90afa4eb3bb58cfc1ad
Is-This-Merge <mojin81@sasa.hs.kr>
2025-11-30 17:14:35 +0900

    subject

--- static/js/curriculum.js
+++ static/js/curriculum.js
@@ -9,138 +9,165 @@ const curriculums = [
             {
                 id: 1,
                 name: '수학Ⅰ',
+                engname: 'mathematicsI',
                 semester: [1],
                 credits: 2,
                 frequency: 2,
+                in: '전문교과',
                 necessary: '필수',
-                AP: 0,
-                text: {
-                    
-                }
+                AP: 0
             },
             {
                 id: 2,
                 name: '수학Ⅱ',
+                engname: 'mathematicsII',
                 semester: [1],
                 credits: 3,
                 frequency: 3,
+                in: '전문교과',
                 necessary: '필수',
                 AP: 0
             },
             {
                 id: 3,
                 name: '수학Ⅲ',
+                engname: 'mathematicsIII',
                 semester: [2],
                 credits: 2,
                 frequency: 2,
+                in: '전문교과',
                 necessary: '필수',
                 AP: 0
             },
             {
                 id: 4,
                 name: '수학Ⅳ',
+                engname: 'mathematicsIV',
                 semester: [2],
                 credits: 3,
                 frequency: 3,
+                in: '전문교과',
                 necessary: '필수',
                 AP: 0
             },
             {
                 id: 5,
                 name: '확률과 통계',
+                engname: 'Probability-and-Statistics',
                 semester: [3],
                 credits: 3,
                 frequency: 3,
+                in: '전문교과',
                 necessary: '필수',
                 AP: 0
             },
             {
                 id: 6,
                 name: '미적분과 해석기하',
+                engname: 'Calculus-and-Analytic-Geometry',
                 semester: [3],
                 credits: 3,
                 frequency: 3,
+                in: '전문교과',
                 necessary: '필수',
                 AP: 0
             },
             {
                 id: 7,
                 name: '미적분학Ⅰ',
+                engname: 'CalculusI',
                 semester: [4, 6],
                 credits: 3,
                 frequency: 4,
+                in: '전문교과',
                 necessary: '기본선택',
                 AP: 1
             },
             {
                 id: 8,
                 name: '이산수학',
+                engname: 'Discrete-Mathematics',
                 semester: [4, 6],
                 credits: 3,
                 frequency: 3,
+                in: '전문교과',
                 necessary: '자율선택',
                 AP: 0
             },
             {
                 id: 9,
                 name: '정수론',
+                engname: 'Number-Theory',
                 semester: [4, 6],
                 credits: 3,
                 frequency: 3,
+                in: '전문교과',
                 necessary: '자율선택',
                 AP: 0
             },
             {
                 id: 10,
                 name: '선형대수학',
+                engname: 'Linear-Algebra',
                 semester: [5],
                 credits: 3,
                 frequency: 4,
+                in: '전문교과',
                 necessary: '자율선택',
                 AP: 1
             },
             {
                 id: 11,
                 name: '미적분학Ⅱ',
+                engname: 'CalculusII',
                 semester: [5, 6],
                 credits: 3,
                 frequency: 4,
+                in: '전문교과',
                 necessary: '자율선택',
                 AP: 1
             },
             {
                 id: 12,
                 name: '수학사 및 수학실험',
+                engname: 'History-of-Mathematics-and-Mathematical-Experiments',
                 semester: [3, 5],
                 credits: 2,
                 frequency: 2,
+                in: '창의융합교과',
                 necessary: '자율선택',
                 AP: 0
             },
             {
                 id: 13,
                 name: '융합@수학',
+                engname: 'Interdisciplinary-Mathematics',
                 semester: [4, 6],
                 credits: 2,
                 frequency: 2,
+                in: '창의융합교과',
                 necessary: '자율선택',
                 AP: 0
             },
             {
                 id: 14,
                 name: '수학적 문제해결과 의사소통Ⅰ',
+                engname: 'Mathematical-Communication-and-Problem-Solving-StrategyI',
                 semester: [5],
                 credits: 3,
                 frequency: 3,
+                in: '창의융합교과',
                 necessary: '기본선택',
                 AP: 0
             },
             {
                 id: 15,
                 name: '수학적 문제해결과 의사소통Ⅱ',
+                engname: 'Mathematical-Communication-and-Problem-Solving-StrategyI',
                 semester: [6],
                 credits: 3,
                 frequency: 3,
+                in: '창의융합교과',
                 necessary: '기본선택',
                 AP: 0
             }
